c87ea5a2bce4f917020521e000d05fd1cbf1ec3a
matrix <gopher997@gmail.com>
2024-05-14 04:57:34 +0800

    提交Go入门程序示例代码

---
+++ 01-基础部分/01-入门程序/hello.go
@@ -0,0 +1,11 @@
+// 声明当前go程序所在的包
+package main
+
+// 引入了fmt来使用，fmt包提供了格式化输入输出的函数。
+import "fmt"
+
+// main函数是整个go程序的主函数，是程序执行的起点。
+func main() {
+	// 调用了fmt包的Println函数，输出字符串 "Hello World!"。
+	fmt.Println("Hello World!")
+}
